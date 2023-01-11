d91e5aab7f4ef554df8772c51c5f1eafca59cbd7
snackli17 <87991354+snackli17@users.noreply.github.com>
2023-01-11 22:22:58 +0000

    Deploying to gh-pages from @ snackli17/goit-js-hw-09@15dee2b02b39371a8dd0cca37edece628edbca5c 🚀

---
+++ 01-color-switcher.86d71db1.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),r=document.querySelector("body");let o=null;e.addEventListener("click",(()=>{o=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),e.setAttribute("disabled",!0)})),t.addEventListener("click",(()=>{clearInterval(o),e.removeAttribute("disabled")}));
+//# sourceMappingURL=01-color-switcher.86d71db1.js.map
